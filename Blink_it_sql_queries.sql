38aeaf889bf1361028c7294ea4a4a67ed234d211
Kanimozhi V <146629187+Kani-mozhi3@users.noreply.github.com>
2026-01-31 12:59:34 +0530

    Update Blink_it_sql_queries.sql

--- Blink_it_sql_queries.sql
+++ Blink_it_sql_queries.sql
@@ -1,1 +1,65 @@
-Sql queries
+
+--Q1.
+SELECT CAST(SUM(Total_Sales) / 1000000.0 AS DECIMAL(10,2)) AS Total_Sales_Million
+FROM blinkit_data;
+
+
+--Q2.
+SELECT CAST(AVG(Total_Sales) AS INT) AS Avg_Sales
+FROM blinkit_data;
+
+
+--Q3.
+SELECT COUNT(*) AS No_of_Orders
+FROM blinkit_data;
+
+
+--Q4.
+SELECT CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating
+FROM blinkit_data;
+
+
+--Q5.
+SELECT Item_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Item_Type
+ORDER BY Total_Sales DESC
+
+  
+--Q6.
+SELECT Item_Fat_Content, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Item_Fat_Content
+
+  
+--Q7.
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales) 
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+ORDER BY Outlet_Location_Type;
+
+
+--Q8.
+SELECT Outlet_Establishment_Year, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Establishment_Year
+ORDER BY Outlet_Establishment_Year
+
+
+--Q9.
+SELECT Outlet_Location_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Location_Type
+ORDER BY Total_Sales DESC
